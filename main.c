e9ab28207d1d5fb0480296950e6d0402c353c92f
hilal.kurulay@smail.th-koeln.de <hilal.kurulay@smail.th-koeln.de>
2021-05-23 15:18:08 +0200

    Start

--- main.c
+++ main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
-
-int main() {
-    printf("Hello, World!\n");
-    //test
-    //testmert
-    return 0;
-}
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <sys/wait.h>
+#define BUFFSIZE 256 // Größe des Buffers
+#define STORAGESIZE 5 // Größe des Storage
+#define TRUE 1
+#define ENDLOSSCHLEIFE 1
+#define PORT 5678
+#define SEGSIZE 10000
